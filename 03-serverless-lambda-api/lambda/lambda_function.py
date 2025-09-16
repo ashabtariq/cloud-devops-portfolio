9d2afefb7f706faeef52877bfd5dadd2e611aa77
ashab.tariq <ashab.tariq@hotmail.com>
2025-09-17 00:38:48 +0700

    Modified lambda function to extract request body

--- 03-serverless-lambda-api/lambda/lambda_function.py
+++ 03-serverless-lambda-api/lambda/lambda_function.py
@@ -4,15 +4,17 @@ import hashlib
 import boto3
 
 dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('url_shortner_terra')
+table = dynamodb.Table('url_shortner')
+
 
 def lambda_handler(event, context):
-    long_url = event['longUrl']
+    long_url = event.get('body-json').get('longUrl')
     short_code = hashlib.sha256(long_url.encode()).hexdigest()[:8]
     shorten_url = 'http://localhost:5000/'+short_code
     table.put_item(Item={'short_code':short_code,'long_url': long_url,'short_url':  shorten_url})
     return {
         'statusCode': 200,
-        'body': "Record Added"
+        'body': shorten_url
     }
+    print("Hello World")
 
