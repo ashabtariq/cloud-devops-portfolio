b9663ea26fd7d7652934450abcac358a06724a64
ashab.tariq <ashab.tariq@hotmail.com>
2025-09-25 20:10:59 +0700

    New Project Push

---
+++ 06-Container-Secuirty-Pipeline/api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+@app.get("/test")
+async def root():
+    return {"message": "Endpoint Is Working "}
