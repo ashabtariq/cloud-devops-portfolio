9ad1e1c4028257466cd6c9ce40eb9255139080a8
Ashab Tariq <ashab.tariq@hotmail.com>
2025-10-08 20:26:53 +0700

    Added a remote backenf for production like scenario

--- 07-Secure-VPC-with-Public-Private-Subnets/main.tf
+++ 07-Secure-VPC-with-Public-Private-Subnets/main.tf
@@ -2,6 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
+
+terraform {
+  backend "s3" {
+    bucket         = "buildeedev-tfstate-dev"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "buildeedev-tf-locks-dev"
+    encrypt        = true
+  }
+}
+
+
 # -------------------------
 # VPC
 # -------------------------
